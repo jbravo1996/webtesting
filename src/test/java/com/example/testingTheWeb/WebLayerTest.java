2141e353dc9e5628763f7ae0f6bfd89fa7d727fb
Javier Bravo <jrbv@gft.com>
2022-06-15 15:43:40 +0200

    minor fixes

--- src/test/java/com/example/testingTheWeb/WebLayerTest.java
+++ src/test/java/com/example/testingTheWeb/WebLayerTest.java
@@ -4,7 +4,6 @@ package com.example.testingTheWeb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
