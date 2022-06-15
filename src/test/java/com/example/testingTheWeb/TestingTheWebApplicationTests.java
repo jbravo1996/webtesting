c706139284cc6b6ae335dbed7e4db475a9d00db7
Javier Bravo <106956011+jbravo1996@users.noreply.github.com>
2022-06-15 12:04:00 +0200

    minor fix

--- src/test/java/com/example/testingTheWeb/TestingTheWebApplicationTests.java
+++ src/test/java/com/example/testingTheWeb/TestingTheWebApplicationTests.java
@@ -15,7 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TestingTheWebApplicationTestsnTests {
+public class TestingTheWebApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
