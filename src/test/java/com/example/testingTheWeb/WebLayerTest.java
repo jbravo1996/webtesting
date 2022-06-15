18960d8b468662d4027c0f6bb5780645c138fc83
Javier Bravo <jrbv@gft.com>
2022-06-15 15:35:33 +0200

    minor fixes

--- src/test/java/com/example/testingTheWeb/WebLayerTest.java
+++ src/test/java/com/example/testingTheWeb/WebLayerTest.java
@@ -4,6 +4,7 @@ package com.example.testingTheWeb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -18,6 +19,9 @@ public class WebLayerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+	@MockBean
+	private GreetingService service;
 
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
