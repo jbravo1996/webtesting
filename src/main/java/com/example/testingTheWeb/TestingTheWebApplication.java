c22b85900121b374962cfe60d545f7d38314502a
Javier Bravo <jrbv@gft.com>
2022-06-15 10:37:35 +0200

    first commit

---
+++ src/main/java/com/example/testingTheWeb/TestingTheWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.testingTheWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingTheWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingTheWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testingTheWeb/TestingTheWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingTheWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingTheWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
