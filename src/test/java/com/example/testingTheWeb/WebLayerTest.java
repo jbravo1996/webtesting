fbc60861be8eae34bb5bcaacb16f6b9a756541ab
Javier Bravo <106956011+jbravo1996@users.noreply.github.com>
2022-06-15 12:31:12 +0200

    minor fixes

--- src/test/java/com/example/testingTheWeb/WebLayerTest.java
+++ src/test/java/com/example/testingTheWeb/WebLayerTest.java
@@ -12,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@SpringBootConfiguration
 public class WebLayerTest {
 
 	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
