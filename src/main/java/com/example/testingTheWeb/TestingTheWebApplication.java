2b8d178f342cadeca16b613fa1c536bff0e45d22
Javier Bravo <jrbv@gft.com>
2022-06-16 16:47:12 +0200

    added a new plugin + new mappings + - * / + way more test cases

--- src/main/java/com/example/testingTheWeb/TestingTheWebApplication.java
+++ src/main/java/com/example/testingTheWeb/TestingTheWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingTheWebApplication {
 
+	@Generated
 	public static void main(String[] args) {
 		SpringApplication.run(TestingTheWebApplication.class, args);
 	}
